ca683733f7d03b11dda4787f5414df9064c192c0
Tống Đại <raznopro2k5@gmail.com>
2025-04-25 02:05:46 +0000

    hi

--- mytest/src/App.java
+++ mytest/src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("dai say hello, World!");
+        System.out.println("dai hihihi");
+        
     }
 }

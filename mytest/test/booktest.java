f31e186b1f1dd055e585397647f12e2f5faa3e36
Tống Đại <raznopro2k5@gmail.com>
2025-05-15 02:22:53 +0000

    tongdai1

---
+++ mytest/test/booktest.java
@@ -0,0 +1,5 @@
+public class booktest {
+    public static void main(String[] args) {
+        // Tạo sách
+        Book book = new Book("B001", "Lập trình Java", "Nguyễn Văn A");
+        System.out.println(book.inchuoiString());
---
+++ mytest/test/usertest.java
@@ -0,0 +1,6 @@
+public class usertest {
+    public static void main(String[] args) {
+        User user = new User("U001", "Trần Thị B");
+        System.out.println(user.inchuoiString());
+    }
+}

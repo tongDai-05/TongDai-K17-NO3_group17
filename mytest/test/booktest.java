4968858383c3e118b8d9192db2e88b78a8df9e23
Tống Đại <raznopro2k5@gmail.com>
2025-05-15 02:24:08 +0000

    3333

--- mytest/test/booktest.java
+++ mytest/test/booktest.java
@@ -1,7 +1,7 @@
 public class booktest {
     public static void main(String[] args) {
         // Tạo sách
-        Book book = new Book("B001", "Lập trình Java", "Nguyễn Văn A");
+        Book book = new Book("B001", "Lập trình Java", "Tạ Thị Đại");
         System.out.println(book.inchuoiString());
     }
 }

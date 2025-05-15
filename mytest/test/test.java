aa7efb8623d46b24b951cbd163eec8075e48f587
Tống Đại <raznopro2k5@gmail.com>
2025-05-15 01:44:00 +0000

    tongdai

--- mytest/test/test.java
+++ mytest/test/test.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 import java.util.Scanner;
 
 public class test {
-    public static void main(String[] args) {
+    public static void test(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         // Nhập thông tin người dùng

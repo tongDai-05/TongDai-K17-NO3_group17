0f00344f2a3338478c3c0aa9e3fa7ff7edf7c73c
Tống Đại <raznopro2k5@gmail.com>
2025-05-16 00:29:45 +0000

    togdai11

---
+++ mytest/review/Flower.java
@@ -0,0 +1,14 @@
+package mytest.review;
+public class Flower {
+ int petalCount = 0;
+ String s = new String("null");
+ Flower(int petals) { petalCount = petals; }
+ Flower(String ss) { s = ss; }
+ Flower(String s, int petals) {
+ this(petals);
+//! this(s); // can’t do it twice
+ this.s = s;
+ }
+ Flower() { this("hi", 47); } // default constructor
+ 
+}
---
+++ mytest/review/Main.java
@@ -0,0 +1,9 @@
+package mytest.review;
+
+public class Main {
+    public static void main(String[] args) {
+        Flower f = new Flower();
+        System.out.println("Petal count: " + f.petalCount);
+        System.out.println("s: " + f.s);
+    }
+}

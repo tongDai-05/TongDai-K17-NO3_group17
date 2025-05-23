54fea230b2f6c45ac1b09aede0cf50b76be31ab6
phuonganh <151888704+Phanh231@users.noreply.github.com>
2025-05-23 08:58:58 +0700

    Update UserBook.java

--- qli_thuvien/src/UserBook.java
+++ qli_thuvien/src/UserBook.java
@@ -1,5 +1,39 @@
 package qli_thuvien.src;
+import java.util.ArrayList;
 
 public class UserBook{
+    ArrayList<Book> list= new ArrayList<Book>(); // them danh sach
+    public ArrayList<Book> addBook(Book ub){
+        list.add(ub);
+        return list;
+    }
 
+// sua sach
+
+    public ArrayList<Book> getEditBook(String masach,String tensach ){  
+        for(int i=0; i < list.size(); i++){
+            if(list.get(i).masach == masach){
+                System.out.print("true");
+                list.get(i).tensach= tensach; 
+            }
+            
+        }
+        return list;
+    }
+// xoa sach
+    public ArrayList<Book> getDeleBook(String ms){
+        for(int i=0; i< list.size(); i++){
+            if(list.get(i).masach == ms){
+            list.remove(i);
+        }
+      } return list;
+    }
+
+ // doc
+public void printUserBook(){
+    int len = list.size();
+    for(int i=0; i< len; i++){
+        System.out.println("[" +list.get(i).masach + "]" +list.get(i).tensach+ "-" + list.get(i).trangthaiMuon);
+    }
+ }
 }

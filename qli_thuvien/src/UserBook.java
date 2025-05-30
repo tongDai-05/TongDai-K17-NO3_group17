3522bb503b2e4c1a939f79f13044df43bfd330c9
phuonganh <151888704+Phanh231@users.noreply.github.com>
2025-05-30 02:53:42 +0000

    test loanrecord

--- qli_thuvien/src/UserBook.java
+++ qli_thuvien/src/UserBook.java
@@ -1,15 +1,17 @@
 package qli_thuvien.src;
 import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class UserBook{
     ArrayList<Book> list= new ArrayList<Book>(); // them danh sach
+    ArrayList<LoanRecord> dsm= new ArrayList<>(); // danh sach ban ghi muon
+
     public ArrayList<Book> addBook(Book ub){
         list.add(ub);
         return list;
     }
 
 // sua sach
-
     public ArrayList<Book> getEditBook(String masach,String tensach ){  
         for(int i=0; i < list.size(); i++){
             if(list.get(i).masach == masach){
@@ -36,4 +38,30 @@ public void printUserBook(){
         System.out.println("[" +list.get(i).masach + "]" +list.get(i).tensach+ "-" + list.get(i).trangthaiMuon);
     }
  }
+
+ //muonsach
+    public void muonSach(String maSach, User user, LocalDate ngayMuon) {
+        for (Book b : list) {
+            if (b.masach.equals(maSach) && !b.trangthaiMuon) {
+                b.muonsach();  // Đánh dấu là đã mượn
+                LoanRecord record = new LoanRecord(b, user, ngayMuon);
+                dsm.add(record);  // Thêm vào danh sách đã mượn
+                System.out.println("[" + b.masach + "] đã được mượn bởi " + user.getName()+ " vào ngày " + ngayMuon);
+                return;
+            }
+        }
+        System.out.println("Khong tim thay sach hop le hoac sach da duoc muon.");
+    }
+
+    // Hiển thị danh sách sách được mượn theo ngày
+    public void hienThiSachMuonTheoNgay(LocalDate ngay) {
+        System.out.println("Danh sach duoc muon vao ngay: " + ngay);
+        for (LoanRecord lr : dsm) {
+            if (lr.getBorrowDate().equals(ngay)) {
+                System.out.println("- " + lr.getBook().tensach + " (Nguoi muon: " + lr.getUser().getName() + ")");
+            }
+        }
+    }
+
+
 }
---
+++ qli_thuvien/test/ListOfUser.java
@@ -0,0 +1,5 @@
+package qli_thuvien.test;
+
+public class ListOfUser {
+    
+}

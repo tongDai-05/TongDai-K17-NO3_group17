384c2ebe0b515beccdd0e540f7b7480c4384c8b9
Tống Đại <raznopro2k5@gmail.com>
2025-05-31 08:53:47 +0000

    ước chạy dc

---
+++ myJavaProject/TEST/UserBook.java
@@ -0,0 +1,6 @@
+public class UserBook {
+    public static void run(String[] args) {
+        UserBook ub = new UserBook("B01", "U01", "2025-05-09");
+        ub.displayBorrowInfo();
+    }
+}
--- myJavaProject/src/Main.java
+++ myJavaProject/src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== BẮT ĐẦU TEST ===");
         chuyensachTest.run();
+        BookTest.run();
+        UserBook.run();
+        UserTest.run();
         System.out.println("=== KẾT THÚC TEST ===");
     }
 }

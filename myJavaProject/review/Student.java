133cf502b1384eda4f4b28f120fc8ea8006023f9
Tống Đại <raznopro2k5@gmail.com>
2025-06-05 01:27:40 +0000

    up

--- myJavaProject/review/Student.java
+++ myJavaProject/review/Student.java
@@ -9,32 +9,18 @@ public class Student implements Comparable {
 
     public Student() {}
 
-    public String getName() {
-        return name;
-    }
-
-    public float getGpa() {
-        return gpa;
-    }
-
-    @Override
-    public int compareTo(Object o) {
-        if (!(o instanceof Student)) return -1;
-        Student other = (Student) o;
-        if (other.gpa < gpa) return 1;
-        else if (other.gpa > gpa) return -1;
-        else return 0;
-    }
-
-    @Override
     public boolean equals(Object o) {
-        if (!(o instanceof Student)) return false;
-        Student other = (Student) o;
-        return this.gpa == other.gpa;
+        if (gpa == ((Student) o).gpa) return true;
+        else return false;
     }
 
-    @Override
-    public int hashCode() {
-        return Float.valueOf(gpa).hashCode();
+    public String getName() { return name; }
+
+    public float getGpa() { return gpa; }
+
+    public int compareTo(Object o) {
+        if (((Student)o).gpa < gpa) return 1;
+        else if (((Student)o).gpa > gpa) return -1;
+        else return 0;
     }
 }
--- myJavaProject/review/TestStudent.java
+++ myJavaProject/review/TestStudent.java
@@ -6,20 +6,16 @@ public class TestStudent {
         Student s2 = new Student("Sam", 3.5F);
         Student s3 = new Student("Steve", 2.1F);
 
-        // So sánh GPA giữa s3 và s2
-        if (s3.compareTo((Object) s2) < 0)
-            System.out.println(s3.getName() + " has a lower GPA than " + s2.getName());
+        if (s3.compareTo((Object)s2) < 0)
+            System.out.println(s3.getName() + " has a lower gpa than " + s2.getName());
 
-        // Tạo TreeSet và thêm sinh viên vào
         Set studentSet = new TreeSet();
         studentSet.add(s1);
         studentSet.add(s2);
         studentSet.add(s3);
 
-        // Duyệt qua Set bằng Iterator
         Iterator i = studentSet.iterator();
-        System.out.println("Students sorted by GPA:");
         while (i.hasNext())
-            System.out.println(((Student) i.next()).getName());
+            System.out.println(((Student)i.next()).getName());
     }
 }

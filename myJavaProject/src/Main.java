46a04b1ee90ead9a8a6477c933f7005d70e818a7
Tống Đại <raznopro2k5@gmail.com>
2025-06-06 00:46:12 +0000

    tongg daiiii

--- myJavaProject/src/Main.java
+++ myJavaProject/src/Main.java
@@ -2,9 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("=== BẮT ĐẦU TEST ===");
         MuonSachTest.run();
-        BookTest.run(args);
-        UserBookTest.run(args);
-        UserTest.run(args);
         System.out.println("=== KẾT THÚC TEST ===");
     }
 }

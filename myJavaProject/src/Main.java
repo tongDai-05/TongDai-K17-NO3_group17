837696436f45d87bbd01b85734d0570b30366d54
Tống Đại <raznopro2k5@gmail.com>
2025-06-19 00:03:17 +0000

    upppp1112

--- myJavaProject/src/Main.java
+++
@@ -1,7 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("=== Library ===");
-        MuonSachTest.run();
-        System.out.println("=== Cảm ơn bạn đọc ===");
-    }
-}

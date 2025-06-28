8d7ebf4d8fa84b7831cbac7bd83d78a7a4c1d5f3
Phuonganh005 <phamthiphuonganh2315@gmail.com>
2025-06-28 00:39:16 +0000

    update

--- gs-serving-web-content-main/gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/database/BorrowBookAiven.java
+++ gs-serving-web-content-main/gs-serving-web-content-main/complete/src/main/java/com/example/servingwebcontent/database/BorrowBookAiven.java
@@ -7,8 +7,8 @@ import java.sql.PreparedStatement;
 public class BorrowBookAiven {
     public void borrowBook(String bookID) {
         try (Connection conn = DriverManager.getConnection(
-                "jdbc:mysql://...your_url_here...",
-                "avnadmin", "your_password")) {
+                "jdbc:mysql://avnadmin:AVNS_4nU1y6avZRmSrZ0OhWl@mysql-cd52b85-raznopro2k5-c07e.c.aivencloud.com:22996/defaultdb?ssl-mode=REQUIRED",
+                "avnadmin", "AVNS_4nU1y6avZRmSrZ0OhWl")) {
 
             String query = "UPDATE book SET borrowed = TRUE WHERE bookID = ?";
             PreparedStatement pst = conn.prepareStatement(query);
